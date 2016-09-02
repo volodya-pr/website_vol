9490bfbf7890ac220df5d8f8e00cf5e2e583dcbe
Volodya <prokopiv.v@gmail.com>
2016-09-02 22:34:28 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -7,3 +7,17 @@ $(function(){
 		$('body,html').animate({scrollTop: top}, 800);
 	});
 });
+
+$("#texth1").fadeIn(1000);
+
+$(function(){
+2
+        $(window).scroll(function(){
+3
+            var bo = $("#texth1").scrollTop();
+4
+        if ( bo > 200 ) $("#home").animate({'opacity':'1'},2);
+5
+        })
+6
+    })
