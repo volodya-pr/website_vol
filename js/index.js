c10a67624c9f36ea7e433bae3d8fb6931bc9f297
Volodya <prokopiv.v@gmail.com>
2016-09-03 21:47:41 +0000

    add new file

--- js/index.js
+++ js/index.js
@@ -11,13 +11,65 @@ $(function(){
 $("#texth1").fadeIn(1000);
 
 $(function(){
-2
+
         $(window).scroll(function(){
-3
+
             var bo = $("#texth1").scrollTop();
-4
+
         if ( bo > 200 ) $("#home").animate({'opacity':'1'},2);
-5
+
         })
-6
+
     })
+/**
+ * modelne vikno
+ */
+$(document).ready(function() {
+	$('a[name=modal]').click(function(e) {
+		e.preventDefault();
+		var id = $(this).attr('href');
+		var maskHeight = $(document).height();
+		var maskWidth = $(window).width();
+		$('#mask').css({'width':maskWidth,'height':maskHeight});
+		$('#mask').fadeIn(1000);
+		$('#mask').fadeTo("slow",0.8);
+		var winH = $(window).height();
+		var winW = $(window).width();
+		$(id).css('top',  winH/2-$(id).height()/2);
+		$(id).css('left', winW/2-$(id).width()/2);
+		$(id).fadeIn(2000);
+	});
+	$('.window .close').click(function (e) {
+		e.preventDefault();
+		$('#mask, .window').hide();
+	});
+	$('#mask').click(function () {
+		$(this).hide();
+		$('.window').hide();
+	});
+});
+
+$(function() {
+    $('#btn-send').click(sendForm);
+
+})
+
+function sendForm(e) {
+    e.preventDefault();
+    $.ajax({
+        url: "https://formspree.io/prokopiv.v@gmail.com",
+        method: "POST",
+        data: {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            message: $('#msg').val(),
+            rem: $('#rem').val(),
+            vid: $('#vid').val()
+        },
+        dataType: "json",
+        success: function() {
+            $('#thanks').html('thanks');
+        }
+    });
+
+}
